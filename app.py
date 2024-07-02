af55cdd55d5fa951b819ca4ba58d50f559f5bcc9
HARSH SHUKLA <HARSHSHUKLA2022@VITBHOPAL.AC.IN>
2024-07-02 15:17:48 +0530

    changes to deploy on vercel

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for, send_file
+from flask import Flask, render_template, request, send_file
 from pytube import YouTube
 import os
+import re
 
 app = Flask(__name__)
+
+def extract_video_id(url):
+    video_id = re.search(r"(?<=v=)[^&]+", url)
+    if not video_id:
+        video_id = re.search(r"(?<=be/)[^&]+", url)
+    return video_id.group(0) if video_id else None
 
 @app.route('/')
 def index():
@@ -13,11 +20,12 @@ def fetch():
     url = request.form['url']
     try:
         yt = YouTube(url)
+        video_id = extract_video_id(url)
         video_details = {
             'title': yt.title,
             'streams': yt.streams.filter(progressive=True) if request.form['download_type'] == 'video' else yt.streams.filter(only_audio=True)
         }
-        return render_template('download.html', video_details=video_details, download_type=request.form['download_type'])
+        return render_template('download.html', video_details=video_details, download_type=request.form['download_type'], video_id=video_id, url=url)
     except Exception as e:
         return f"An error occurred: {e}"
 
@@ -36,6 +44,3 @@ def download():
         return send_file(download_path, as_attachment=True)
     except Exception as e:
         return f"An error occurred: {e}"
-
-if __name__ == '__main__':
-    app.run(debug=True)
